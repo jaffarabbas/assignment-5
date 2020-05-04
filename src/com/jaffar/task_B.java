a6d7ff403b5edc0157c2342c64cf5427fa526872
Jaffar Abbas <gamaportal8@gmail.com>
2020-05-04 20:53:30 +0500

    5-4-2020

---
+++ src/com/jaffar/task_B.java
@@ -0,0 +1,52 @@
+package com.jaffar;
+
+ class task_B {
+    public static void main(String[] args) {
+        MailMessage send1 = new MailMessage();
+        send1.SendMailMessage();
+    }
+}
+
+class MailMessage{
+    private Header header;
+    private Body bd;
+    private Attachment attachment;
+
+    MailMessage(){
+        header = new Header();
+        bd = new Body();
+        attachment = new Attachment();
+    }
+
+    public void SendMailMessage(){
+        System.out.println(toString());
+    }
+
+    public String toString(){
+        return (header.getTitle()+" "+bd.getMessage()+" "+attachment.getFileName());
+    }
+}
+
+class Header{
+    public String Title =   "This is the Header of the mail message";
+
+    public String getTitle() {
+        return Title;
+    }
+}
+
+class Body{
+    public String message =   "This is the Body of the mail message";
+
+    public String getMessage() {
+        return message;
+    }
+}
+
+class Attachment{
+    public String fileName =   "This is the Attachment of the mail message";
+
+    public String getFileName() {
+        return fileName;
+    }
+}
